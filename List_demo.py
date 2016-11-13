1f9c37b8bdf82194bc9f2c991d31b8baca6e14d7
Judice <Lancelot@zhengjiandeMacBook-Pro.local>
2016-11-14 01:34:12 +0800

    first commit

--- List_demo.py
+++ List_demo.py
@@ -6,3 +6,11 @@ for i in mylist:
 mygenerator = (x*x for x in range(3)) #生成器, []变为()
 for i in mygenerator:
     print i
+
+def createGenarator():  #yield的使用,创建生成器
+    for i in range(3):
+        yield i*i
+
+mygenerator = createGenarator() #创建生成器实例
+for i in mygenerator:
+    print i
